52e2921ff0cdda325ef9a610285c9373a2accf27
bichanna <nobu.bichanna@gmail.com>
2022-01-22 20:37:28 -0600

    Get user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("This is guessing game program!");
+    let mut guess = String::new();
+    println!("Guess a number");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("You guesses: {}", guess);
 }
